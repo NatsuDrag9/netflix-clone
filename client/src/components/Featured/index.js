a78a3d388031fb961a3964f7b5e8b6069e4698ce
NatsuDrag9 <rohitimandi9@zoho.com>
2023-11-19 21:14:12 +0100

    Removed access tokens during authentication

--- client/src/components/Featured/index.js
+++ client/src/components/Featured/index.js
@@ -16,7 +16,6 @@ function Featured({ type, setGenre }) {
         const response = await axios.get(`/movies/random?type=${type}`, {
           headers: {
             token: "Bearer" + JSON.parse(localStorage.getItem("user")).accessToken,
-              // "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NTI0MDRlMjc2ZmEzMDQyYTEzOWY2ZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY5OTk5MzM3NiwiZXhwIjoxNzAwNDI1Mzc2fQ.XDEwBBOU7xo4vbKoPdmMSIZKTwi5v81vxuIy03R5Gx8",
           },
         });
         setContent(response.data[0]);
