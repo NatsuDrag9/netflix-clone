8fe2220bbb238ec02865122966f756b19b41b810
NatsuDrag9 <rohitimandi9@zoho.com>
2023-11-24 18:55:50 +0100

    Added cors for vercel deployment

--- server/index.js
+++ server/index.js
@@ -13,19 +13,31 @@ mongoose
   .then(() => console.log("DB connection successfull!"))
   .catch((err) => console.log(err));
 
-// async function main() {
-//   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-//   await mongoose.connect(process.env.MONGO_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("DB connection successfull!")
-//   .catch((err) => console.log(err)));
-// }
+/* async function main() {
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  await mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection successfull!")
+  .catch((err) => console.log(err)));
+} */
 
+// Using express server
 // Make express server accept json files
 app.use(express.json());
 
+// Define CORS options
+const corsOptions = {
+  origin: 'https://natsudrag9.github.io/netflix-clone/',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
+
+// Enabling CORS for all routes
+app.use(cors(corsOptions));
+
+/* API calls */
 // Post request made to the "/app/auth" end point
 // belonging to authRoute
 app.use("/api/auth", authRoute);
@@ -40,7 +52,15 @@ app.use("/api/movies", movieRoute);
 // CRUD operation requests made to "/api/lists" end point
 app.use("/api/lists", listRoute);
 
-// app.listen(8800, () => {
-//   // Listens on the port 8800 on localhost
-//   console.log("Backend server is running");
-// });
+// Listening port on localhost
+/* app.listen(8800, () => {
+  // Listens on the port 8800 on localhost
+  console.log("Backend server is running");
+}); */
+
+// Vercel dynamically assigns a port and sets the PORT environment variable
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
