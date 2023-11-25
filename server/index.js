d7632129a27f95814a1d3e867564221370f47330
NatsuDrag9 <rohitimandi9@zoho.com>
2023-11-25 10:46:29 +0100

    Added csp again

--- server/index.js
+++ server/index.js
@@ -39,13 +39,13 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Content security policy
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     "Content-Security-Policy",
-//     "default-src 'self' https://natsudrag9.github.io/netflix-clone/"
-//   );
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self' https://natsudrag9.github.io/netflix-clone/"
+  );
+  next();
+});
 
 /* API calls */
 // Post request made to the "/app/auth" end point
